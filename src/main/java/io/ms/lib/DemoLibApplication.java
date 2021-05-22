386a9a25f7eae7511334e9f17ba90d234366d19b
vishnumrk <vishnumrk@gmail.com>
2021-05-22 17:32:23 +0530

    Initial commit

---
+++ src/main/java/io/ms/lib/DemoLibApplication.java
@@ -0,0 +1,13 @@
+package io.ms.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ms/lib/DemoLibApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ms.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
